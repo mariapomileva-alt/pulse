f52dcf74a02763444b328027b0c696a1385f222a
mariapomileva-alt <maria.pomileva@gmail.com>
2026-01-29 17:10:34 +0200

    Update config.js
    s

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-export const SUPABASE_URL = "https://ygkihopdluebecdvaoqe.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_u0fslqR17Gq6HqK-oi4vRg_ders4QXv";
-export const OWNER_KEY = "CHANGE_ME";
+window.SUPABASE_URL = "https://ygkihopdluebecdvaoqe.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_u0fslqR17Gq6HqK-oi4vRg_ders4QXv";
