71408f84e61eefb184df56519b1b60658396ec61
mariapomileva-alt <maria.pomileva@gmail.com>
2026-02-11 00:13:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL = "https://vngvcbbmniuyuvppcsme.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_pRgu8yb5g28tBscYDK7KBA_5xIpxiAJ";
+window.OWNER_STATS_KEY = "CHANGE_ME";
